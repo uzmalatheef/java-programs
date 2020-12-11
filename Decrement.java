8cf71a8f94c31ac3f9ce7d8896c96da0645a8380
uzmalatheef <75826547+uzmalatheef@users.noreply.github.com>
2020-12-11 12:02:01 +0530

    Inc Dec

---
+++ Decrement.java
@@ -0,0 +1,10 @@
+class Decrement{
+public static void main(String args[]){
+  int num =5;
+  
+  System.out.println("post decrement:" + num--);
+  
+  System.out.println("pre decrement:" + --num);
+
+}
+}
---
+++ Increment.java
@@ -0,0 +1,10 @@
+class Increment{
+public static void main(String args[]){
+  int num =5;
+  
+  System.out.println("post increment:" + num++);
+  
+  System.out.println("pre increment:" + ++num);
+
+}
+}
