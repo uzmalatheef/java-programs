2dd7187bd2caed6749474412f43fb80e79c69942
uzmalatheef <75826547+uzmalatheef@users.noreply.github.com>
2020-12-11 12:30:29 +0530

    Assignment Operators

---
+++ AssignmentOperators.java
@@ -0,0 +1,43 @@
+class AssignmentOperators{
+      public static void main(String args[]){
+   
+   int sum = 10;
+   sum = sum+10;
+    System.out.println("the value of sum is:" + sum);
+   sum += 20;
+    System.out.println("the value of sum is:" + sum);
+   
+   int sub=100;
+   sub = sub-10;
+    System.out.println("the value of sub is:" + sub);
+   sub -=10;
+    System.out.println("the value of sub is:" + sub);
+   
+    int mul = 5;
+	mul = mul*10;
+	 System.out.println("the value of mul is:" + mul);
+    mul *= 10;
+	 System.out.println("the value of mul is:" + mul);
+	 
+	 
+	int div = 10;
+    div = div/20;
+     System.out.println("the value of div is:" + div);
+    div /=25;
+     System.out.println("the value of div:" + div);	 
+ 
+ int mod = 20;
+ mod = mod%5;
+  System.out.println("the value of mod is:" + mod);
+ mod %=20;
+  System.out.println("the value of mod is:" + mod);
+ 
+
+
+
+}
+
+
+
+
+}
